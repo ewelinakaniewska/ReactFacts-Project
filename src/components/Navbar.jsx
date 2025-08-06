1c94e739279113b50c55f8cf5879ba8a0c9134ff
ewelinakaniewska <ekaniewska144@gmail.com>
2025-08-06 22:46:49 +0200

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,9 @@
+import logo from "../assets/react-logo.png";
 export default function Navbar() {
   return (
     <header className="bg-[#21222A] h-[90px] w-[550px] p-[30px]">
       <nav className=" flex flex-row items-center h-full">
-        <img
-          src="/src/assets/react-logo.png"
-          alt="React Logo"
-          className="size-[35px]"
-        />
+        <img src={logo} alt="React Logo" className="size-[35px]" />
         <p className="text-[#61DAFB] font-[700] ml-[7px] text-[1.4rem]">
           ReactFacts
         </p>
